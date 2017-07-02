45844e442b3b6958b6d837c066e497da635a3c09
joe-fuentes <joevfuentes@hotmail.com>
2017-07-02 06:10:32 -0400

    Updated files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,23 +2,25 @@
 // The random number shown at the start of the game should be between 19 - 120.
 var targetNumber = Math.floor(Math.random()*100)+20;
 $("#number-to-guess").text(targetNumber);
-var counter = 0;
 
 /* There will be four crystals displayed as buttons on the page. Each of these crystals will, secretly, be assigned a number valued between 1 - 12. */
 var numberOptions = [ 0, 0, 0, 0];
-var picked = [0, false];
+var counter1 = 0;
+var counter2 = 0;
 
 //To avoid duplication, the values of 1 thru 12 are initialized to false prior to being chosen.
 //Values for each crystal are pre-assigned here.
-/*for (var i; i < numberOptions.length; i++) {
-	if(picked[1] = true) {
-		return;
-	} else {
-		numberOptions[i] = Math.floor(Math.random()*12)+1;
-		picked = [numberOptions[i], true];
+
+//Here, 4 numbers are to be chosen from numbers 1 to 12 to be assigned to each crytals.
+for(counter1 = 0; counter1 < numberOptions.length; counter1++) {
+	for (counter2 = 0; counter2 < numberOptions.lenght; counter2++) {
+		if((numberOptions[counter1] === 0) && (numberOptions[counter1] === numberOptions[counter2])) {
+			numberOptions[counter1] = Math.floor(Math.random()*12)+1;
+			alert(numberOptions[counter1]);
+			console.log(numberOptions[counter1]);
+		}
 	}
-};
-*/
+}
 //Numbers are assigned to each crystal.
 for (var j = 0; j < numberOptions.length; j++) {
 	var crystalImage = $("<img>");
@@ -26,19 +28,19 @@ for (var j = 0; j < numberOptions.length; j++) {
 	crystalImage.attr("src", "assets/images/image" + j.toString() + ".jpg");
     $("#crystals").append(crystalImage);
 	
-     // Each imageCrystal will be given a data attribute called data-crystalValue.
+    // Each imageCrystal will be given a data attribute called data-crystalValue.
     // This data attribute will be set equal to the array value.
     crystalImage.attr("data-crystalvalue", numberOptions[j]);
 }
 /*When the player clicks on a crystal, it will add a specific amount of points to the player's total score.*/
-//for (var k=0; k<numberOptions.length; k++){
-//	alert("crystal value =>", numberOptions[k]);
-//}
-//var crystalValue = ($(this).attr("data-crystalvalue"));
-//   crystalValue = parseInt(crystalValue);
-//$(".crystal-image").on("click", function() {
-//	alert("the number is: " crystalValue);
-//});
+/*for (var k=0; k<numberOptions.length; k++){
+	console.log(numberOptions[k]);
+}*/
+/*var crystalValue = ($(this).attr("data-crystalvalue"));
+	crystalValue = parseInt(crystalValue);
+$(".crystal-image").on("click", function() {
+	alert("the number is: " crystalValue);
+});*/
 /*
 Your game will hide this amount until the player clicks a crystal.
 When they do click one, update the player's score counter.
